502769838be36253ad1877dea95492bb2d421d1e
isaacsinger8 <38815745+isaacsinger8@users.noreply.github.com>
2022-06-23 19:08:21 +0300

    Create TreeTracking.sol
    OEF Research Program Assistant Programming Exercise

---
+++ TreeTracking.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.3;
+
+contract Citizen {
+
+    string public name;
+    string public streetaddress;
+
+    //list of trees the citizen enters; this is a dynamic array but its elements are arrays of fixed size (4)
+    string[4][] public treelist;
+
+ 
+
+    constructor (string memory _name, string memory _streetaddress) {
+        name = _name;
+        streetaddress = _streetaddress;
+    }
+
+    function UpdateName (string memory _newname) public {
+        name = _newname;
+    }
+
+    function UpdateStreetAddress (string memory _newstreetaddress) public {
+        streetaddress = _newstreetaddress;
+    }
+
+    function ViewTreeList() public view returns (string[4][] memory) {
+        return treelist;
+    }
+
+    function ViewTreeRecord(uint _index) public view returns (string[4] memory) {
+        return treelist[_index];
+    }
+
+    function AddTree(string[4] memory _newtree) public {
+        treelist.push(_newtree);
+    }
+
+    function DeleteTree(uint _index) public {
+        //Note: delete does not change the array length, instead the value at _index is reset to zero (default)
+        delete treelist[_index];
+    }
+
+    function UpdateTree(uint _index, uint _internalindex, string memory _newattribute) public view {
+        string[4] memory tree_to_update;
+        tree_to_update = treelist[_index];
+        tree_to_update[_internalindex] = _newattribute;
+    }
+
+
+
+}
